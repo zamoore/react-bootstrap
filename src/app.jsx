ef8bb76dcdd4c84f0e8c944ace78a0c6ae4127cf
Zack <zack@zacks-mbp.lan>
2021-09-24 22:16:56 -0400

    working base

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './components/header/component';
+
+const App = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+export default App;
---
+++ src/components/header/component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Hello</h1>
+  </header>
+);
+
+export default Header;
