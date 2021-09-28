15258d9cf185e5b93dcf7dd6bd2b4a758e15310d
Zack <zack@zacks-mbp.lan>
2021-09-28 15:53:26 -0400

    hot reloading works

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles.css';
 import testImage from './test.jpeg';
+import Button from './button';
 
 const App = () => (
   <div>
     <h1>Hello world</h1>
     <img src={testImage} alt="It's a test" />
+    <Button />
   </div>
 );
 
---
+++ src/button.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Button = () => {
+  const [count, updateCount] = useState(0);
+
+  return (
+    <button
+      type="button"
+      onClick={() => updateCount(count + 1)}
+    >
+      Still still clicked {count} times
+    </button>
+  )
+}
+
+export default Button;
