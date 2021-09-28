138faf9b33e7ef1fdb20208cf1d49986cb9e64c8
Zack <zack@zacks-mbp.lan>
2021-09-28 17:38:38 -0400

    linting and prettier work

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  endfOfLine: 'auto',
+};
--- src/button.tsx
+++ src/button.tsx
@@ -4,13 +4,10 @@ const Button = () => {
   const [count, updateCount] = useState(0);
 
   return (
-    <button
-      type="button"
-      onClick={() => updateCount(count + 1)}
-    >
+    <button type="button" onClick={() => updateCount(count + 1)}>
       Still still clicked {count} times
     </button>
-  )
-}
+  );
+};
 
 export default Button;
