0e94d53a6b919f8b4af7492bf604ce13d3fc3871
Zack <zack@zacks-mbp.lan>
2021-09-28 15:30:54 -0400

    webpack env config working

---
+++ webpack/webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', 'build'),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+  ],
+};
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,46 +1,10 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
 
-const isDevelopment = process.eventNames.NODE_ENV !== 'production';
-
-module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          }
-        ]
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.svg$/i,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '..', 'build'),
-  },
-  mode: 'development',
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-  ],
+module.exports = (envVars) => {
+  const { env } = envVars;
+  const envConfig = require(`./webpack.${env}.js`);
+  const config = merge(commonConfig, envConfig);
+  
+  return config;
 };
