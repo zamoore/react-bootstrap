34983d8342986aa12234dfa37df80a351f94f478
Zack <zack@zacks-mbp.lan>
2021-09-29 11:38:50 -0400

    wip

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import '@app/styles.css';
+import '@app/styles/styles.css';
 import '@app/icon-lib';
 import Home from '@pages/home/page';
 
